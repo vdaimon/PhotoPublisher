25a00a349f6cff0935b0c191852ff7839e817124
Dmitry Vasilev <rcdv13@gmail.com>
2021-11-11 13:38:24 +0300

    Add INPC class

---
+++ PhotoPublisher/INPC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PhotoPublisher
+{
+    public class INPC : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Set<T>(ref T property, T value, Action callback = null, [CallerMemberName] string propName = "")
+        {
+            if ((property == null && value != null) || !property.Equals(value))
+            {
+                property = value;
+
+                if (callback != null)
+                    callback();
+
+                RaisePropertyChanged(propName);
+            }
+        }
+
+        protected void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
--- PhotoPublisher/MainWindow.xaml.cs
+++ PhotoPublisher/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace PhotoPublisher
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
